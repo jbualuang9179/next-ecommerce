b75b6067dd42ed0202dc8c33369aa7d2580a5f64
Jirapong Bualuang <jbualuang21@gmail.com>
2025-09-28 22:07:41 +0700

    first commit

---
+++ app/(main)/page.tsx
@@ -0,0 +1,29 @@
+// 1. rafce
+
+import React from 'react'
+
+// when use async this arrow function
+const page = async() => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default page
+
+
+// 2. --- rfce when use async
+// import React from 'react'
+
+// async function page() {
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+// export default page
+
